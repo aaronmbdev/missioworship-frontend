a311d9bb99bca1a78a949eeaca9412936359bd4f
Marcopole11 <nicomarcopernic@gmail.com>
2023-02-10 18:47:07 +0100

    fix de la variable que falta

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  googleClientId:'726519609743-u96pjemnosno6qs9fal1loq30jk9dd8j.apps.googleusercontent.com',
   API_URL: 'https://api.missio.media/',
   SERVER_ROOT: 'https://www.missio.media/', 
 };
